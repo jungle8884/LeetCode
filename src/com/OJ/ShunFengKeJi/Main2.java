8ef5de6b3cebd3ddb1f6547b55729bcc8de3b057
jungle8884 <jungle8884@163.com>
2021-09-07 00:16:21 +0800

    修改了注释, 理解了优先队列默认是小顶堆

--- src/com/OJ/ShunFengKeJi/Main2.java
+++ src/com/OJ/ShunFengKeJi/Main2.java
@@ -1,10 +1,18 @@
 package com.OJ.ShunFengKeJi;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
+        /*
+        * 输入:
+            6
+            4 -3 -1 -2 1 -5
+        * 输出: 4
+        * 解释: 取 4 -1 -2 1
+        * */
         Scanner reader = new Scanner(System.in);
         // 一共 n 个地方
         int n = reader.nextInt();
@@ -15,18 +23,22 @@ public class Main2 {
         }
         // 开始
         Long cnt = 0L, sum = 0L;
-        // 优先队列-大顶堆: 每次取出最大的负数, 即绝对值最小的(最低降低开心值)
-        PriorityQueue<Long> queue = new PriorityQueue<>();
+        // 优先队列-默认小顶堆: 每次取出最小的负数, 即绝对值最大的, 让 sum 保持最大.
+        PriorityQueue<Long> queue = new PriorityQueue<>(); //默认小顶堆
+//        PriorityQueue<Long> queue = new PriorityQueue<>(n, (a, b)->{return (int) (a - b);}); // 小顶堆
+//        PriorityQueue<Long> queue = new PriorityQueue<>(n, (a, b)->{return (int) (b - a);}); // 大顶堆
         for (int i = 0; i < n; i++) {
             if (sum + arr[i] >= 0) {
                 ++cnt;
                 sum += arr[i];
                 if (arr[i] < 0) {
                     queue.offer(arr[i]);
+//                    System.out.println("加入: " + arr[i]);
+//                    System.out.println(Arrays.toString(queue.toArray()));
                 }
             }  else {
+                // 如果当前元素的 大于 堆顶元素 才可以加进去, 即 绝对值比小顶堆对顶小
                 if (!queue.isEmpty() && queue.peek() < arr[i]) {
-                    // 取出 降低开心值最小的 特色物品
                     sum = sum - queue.peek() + arr[i];
                     queue.poll();
                 }
